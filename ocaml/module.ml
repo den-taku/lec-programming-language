7027913ac9caf8d1d26413d2c6b987f831a81578
DenTaku <dentaku1412@gmail.com>
2020-12-31 03:30:42 +0900

    Learn open token

--- ocaml/module.ml
+++ ocaml/module.ml
@@ -8,3 +8,18 @@ module Sample =
   end
 ;;
   
+(* open Sample;; *)
+
+(* inc Inner.z;; *)
+
+module Sample2 = 
+  struct
+    open Sample.Inner
+    let a = z + 1
+  end
+;;
+
+Sample2.a;;
+(* z;; *)
+
+let open Sample.Inner in z + 100;;
