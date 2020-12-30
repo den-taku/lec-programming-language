73914c435ff3e662f2f6444468a6d98fa8a5b725
DenTaku <dentaku1412@gmail.com>
2020-12-30 23:26:54 +0900

    Test find

--- bstVisitor/Main.java
+++ bstVisitor/Main.java
@@ -7,6 +7,8 @@ public class Main {
         BinarySearchTree t5 = new Branch(new Leaf(), 48, t4);
         BinarySearchTree t6 = new Branch(t3, 30, t5);
         boolean test1 = t6.accept(new Find(30));  // should be true
+        System.out.println("ture -->" + test1);
         boolean test2 = t6.accept(new Find(13));  // should be false
+        System.out.println("false --> "+ test2);
     }
 }
